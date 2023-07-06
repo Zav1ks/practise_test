9f5c3fc6ac1241dced8482611f2ed21559ea13c2
Zav1ks <egorver2001@mail.ru>
2023-07-06 15:51:14 +0300

    Add our test.git/COMMIT_EDITMSG[+] [unix] (15:51 06/07/2023) 1,13 Top -- INSERT -- .git/COMMIT_EDITMSG[+] [unix] (15:51 06/07/2023) 1,13 Top -- INSERT --.git/COMMIT_EDITMSG[+] [unix] (15:51 06/07/2023) 1,13 Top -- INSERT --

---
+++ Test_site.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_driver():
+    return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+def open_page(driver, URL):
+    driver.get(URL)
+
+def get_element_by_id(driver,locator):
+    return driver.find_element(By.ID, locator)
+
+def click_element(driver,locator):
+    element = get_element_by_id(driver,locator)
+    element.click()
+
+def send_keys_element(driver, locator, text):
+    element = get_element_by_id(driver, locator)
+    element.send_keys(text)
+
+def login(driver, LOGIN, PASSWORD):
+    send_keys_element(driver, 'user-name', LOGIN)
+    send_keys_element(driver, 'password', PASSWORD)
+    click_element(driver, 'login-button')
+
+URL = 'https://www.saucedemo.com'
+LOGIN='standard_user'
+#LOGIN='locked_out_user'
+PASSWORD='secret_sauce'
+#ITEM_ID='item_4_title_link'
+driver = get_driver()
+open_page(driver, URL)
+login(driver, LOGIN, PASSWORD)
+#click_element(driver,ITEM_ID)
+driver.quit()
